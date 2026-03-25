b3183037e4f3453e64082539b4b2fd4858d974e4
tanmay <tanmaysawaji9@gmail.com>
2026-03-26 03:35:57 +0530

    5 commit

--- frontend/src/pages/Home/FoodItem.jsx
+++ frontend/src/pages/Home/FoodItem.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FoodItem.css';
 import {assets} from './../../assets/assets.js';
 function FoodItem({id, name ,price, description , image}){
+    const [itemcount , setitemcount] = useState(0);
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className="food-item-img" src={image} alt={name} />
+                {!itemcount
+                    ?<img onClick={()=>setitemcount(prev=>prev+1)} src={assets.add_icon_white}/>
+                    :<div className="food-item-counter">
+                        <img onClick={()=>setitemcount(prev=>prev-1)} src={assets.remove_icon_red}/>
+                        <p>{itemcount}</p>
+                        <img onClick={()=>setitemcount(prev=>prev+1)} src={assets.add_icon_green}/>
+                    </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
