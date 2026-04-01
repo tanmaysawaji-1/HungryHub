d8150ac3769e82c8ad279afc0773b94aad1ab0c5
tanmay <tanmaysawaji9@gmail.com>
2026-04-01 10:50:41 +0530

    9 commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React,{useState} from 'react';
+import React,{useContext, useState} from 'react';
 import './Navbar.css';
 import Logo from '../../assets/logo.png';
 import search_icon from '../../assets/search_icon.png';
 import basket_icon from '../../assets/basket_icon.png';
 import {Link} from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
 function Navbar({setshowlogin}) {
     const [menu, setmenu] = useState("home");
+    const {getTotalCartAmount} = useContext(StoreContext);
+
     return (  
         <div className='navbar'>
             <Link to='/'><img src={Logo} alt="logo" className='logo'/></Link>
@@ -20,7 +23,7 @@ function Navbar({setshowlogin}) {
                     <img src={search_icon} alt=''/>
                     <div className='navbar-search-icon'>
                         <Link to='/cart'><img src={basket_icon} alt='basket'/></Link>
-                        <div className='dot'></div>
+                        <div className={getTotalCartAmount()===0?"":"dot"}></div>
                     </div>
                     <button onClick={()=>setshowlogin(true) }>Sign in </button>
                 </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import './Cart.css';
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 
 function Cart() {
-    const{cartItems, food_list,  renmoveFromCart} = useContext(StoreContext);
-    return (  
+    const{cartItems, food_list,  renmoveFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+    const navigate = useNavigate();
+
+    return ( 
         <>
         <div className='cart'>
             <div className='cart-items'>
@@ -39,25 +43,25 @@ function Cart() {
             <div className="cart-bottom">
                 <div className="cart-total">
                     <h2>Cart Totals</h2>
-                </div>
+                <div>
                 <div className="cart-total-details">
                     <p>Subtotal</p>
-                    <p>{0}</p>
+                    <p>${getTotalCartAmount()}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <p>Delivery Fee</p>
-                    <p>{2}</p>
+                    <p>${getTotalCartAmount()==0?0:2}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <b>Total</b>
-                    <b>{0}</b>
+                    <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
                 </div>
-                
             </div>
-            <button>PROCEED TO CHECKOUT</button>
+            <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
+       
         <div className='cart-promocode'>
             <div>
                 <p>If you have a promo code, Enter it here</p>
@@ -67,7 +71,9 @@ function Cart() {
                 </div>
             </div>
         </div>
-        </>
+        </div>
+        </div>
+    </> 
     );
 }
 
