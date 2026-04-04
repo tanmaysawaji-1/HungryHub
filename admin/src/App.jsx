17ae7f2c9fea4174075866466a5e845b80385707
tanmay <tanmaysawaji9@gmail.com>
2026-04-04 12:05:49 +0530

    14 commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import { Routes , Route } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
 
 function App() {
   return ( 
@@ -9,6 +13,12 @@ function App() {
     <hr/>
     <div className='app-content'>
     <Sidebar/>
+
+    <Routes>
+      <Route path='/add' element={<Add/>}></Route>
+      <Route path='/list' element={<List/>}></Route>
+      <Route path='/orders' element={<Orders/>}></Route>
+    </Routes>
     </div>
     </div>
    );
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Orders.css';
+
+function Orders() {
+    return (  <></>);
+}
+
+export default Orders;
