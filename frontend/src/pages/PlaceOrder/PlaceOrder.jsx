be7302538c4cc0cdb06734a229f9ad6fa34557f3
tanmay <tanmaysawaji9@gmail.com>
2026-05-15 21:51:52 +0530

    commit

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -98,17 +98,17 @@ function PlaceOrder() {
                 <div>
                 <div className="cart-total-details">
                     <p>Subtotal</p>
-                    <p>${getTotalCartAmount()}</p>
+                    <p>₹{getTotalCartAmount()}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <p>Delivery Fee</p>
-                    <p>${getTotalCartAmount()==0?0:2}</p>
+                    <p>₹{getTotalCartAmount()==0?0:2}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <b>Total</b>
-                    <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+                    <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
                 </div>
             </div>
             <button type='submit'>PROCEED TO CHECKOUT</button>
